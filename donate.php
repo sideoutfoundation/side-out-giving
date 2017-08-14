3cb6299eb9e60c01fb78470e77c11844b26e8985
fsimmons <fsimmons@gmail.com>
2017-08-14 10:18:38 -0500

    added

---
+++ donate.php
@@ -0,0 +1,13 @@
+<?php include( './inc/header.php'); ?>
+
+<div class="donate-title">
+	You are donating to
+	<h1>Averill Park High School funds for 2017 Power of Pink Queensburg</h1>
+</div>
+
+<div class="row">
+    <div class="form-wrapper">
+    </div>
+</div>
+
+<?php include( './inc/footer.php'); ?>
---
+++ embed.php
@@ -0,0 +1,20 @@
+<?php include( './inc/header.php'); ?>
+
+
+<div style="width: 400px; margin: 0 auto; padding: 8rem 0;">
+
+	<div class="donate-form">
+
+		<ul class="tabs">
+			<li class="tab-link current" data-tab="tab-1">Donate</li>
+			<li class="tab-link" data-tab="tab-2">Contact</li>
+			<li class="tab-link" data-tab="tab-2">Payment</li>
+		</ul>
+
+		<p>How much would you like to donate?</p>
+
+	</div>
+</div>
+
+
+<?php include( './inc/footer.php'); ?>
