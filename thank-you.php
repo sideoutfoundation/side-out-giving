a4972919fb592c18038e95479370ebd4ecef28a9
fsimmons <fsimmons@gmail.com>
2017-08-14 17:34:53 -0500

    added

---
+++ thank-you.php
@@ -0,0 +1,56 @@
+<?php include( './inc/header.php'); ?>
+
+
+<div style="max-width: 430px; margin: 0 auto; padding: 8rem 0;">
+
+	<div class="donation-form-wrapper">
+
+		<div class="donation-form">
+			<h1>Thank You!</h1>
+			<h4>Your donation was processed</h4>
+			<p>
+				Your generous donation will go a long way
+				to helping us fight stage IV breast cancer.
+				In fact, with your donation, we will be able
+				to fund our <a href="">own clinical trial</a> 
+				and give hopes to those who have lost it. 
+			</p>
+
+
+			<h4>Share your good deed and inspire others to do the same</h4>
+			<div class="share-buttons">
+                <a href="#" class="social social-facebook">
+                    <svg class="icon">
+                        <use xmlns:xlink="http://www.w3.org/1999/xlink"
+                            xlink:href="./img/sprites.svg#icon-facebook">
+                        </use>
+                    </svg>
+                </a>
+                <a href="#" class="social social-twitter">
+                    <svg class="icon">
+                        <use xmlns:xlink="http://www.w3.org/1999/xlink"
+                            xlink:href="./img/sprites.svg#icon-twitter">
+                        </use>
+                    </svg>
+                </a>
+                <a href="#" class="social social-email">
+                    <svg class="icon icon-envelope">
+                        <use xmlns:xlink="http://www.w3.org/1999/xlink"
+                            xlink:href="./img/sprites.svg#icon-envelope">
+                        </use>
+                    </svg>
+                </a>
+            </div>
+
+		</div><!-- .donation-form -->
+
+	</div>
+
+</div>
+
+
+	</div>
+</div>
+
+
+<?php include( './inc/footer.php'); ?>
