a5029221e2173e6b43656f9c6cc632a370341af2
fsimmons <fsimmons@gmail.com>
2017-08-14 00:36:42 -0500

    added

---
+++ inc/pages-menu.php
@@ -0,0 +1,13 @@
+<div style="position: fixed; font-weight: bold; font-size: 12px; background: #1a1a1a; color: yellow; top: 33%; padding: 10px;">
+<ul style="margin: 0; padding: 0;">
+    <li>
+        <a href="sign-up.php">Sign Up</a>
+    </li>
+    <li>
+        <a href="sign-in.php">Sign In</a>
+    </li>
+    <li>
+        <a href="campaign.php">Campaign</a>
+    </li>
+</ul>
+</div>
