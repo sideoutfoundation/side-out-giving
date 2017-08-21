260dcc5b98766625690fe8e6eb8547ea456a22b0
fsimmons <fsimmons@gmail.com>
2017-08-21 03:16:06 -0500

    added

---
+++ join-a-campaign.php
@@ -0,0 +1,143 @@
+<?php include( './inc/header.php'); ?>
+
+<script>
+    document.getElementById('search-link').style.display = 'none';
+    document.getElementById('search-form').style.display = 'block';
+</script>
+
+<h1 class="text-center">Join a Campaign</h1>
+
+    <div class="donate-wrapper">
+
+        <div class="donate-card">
+
+            <img src="./img/demo.jpg" class="img-fluid" height="180">
+
+            <div class="donate-card-inner">
+                <div class="card-description">
+                    <h3>Averill Park High School</h3>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" style="width: 10%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div><!-- .progress -->
+
+                    <div class="progress-stats">
+                        <div class="row">
+                            <div class="col">
+                                $500 Raised (10%)
+                            </div>
+                            <div class="col text-right">
+                                $5000 Goal
+                            </div>
+                        </div>
+                    </div><!-- .progress-stats -->
+
+                    <a href="donate.php" class="btn btn-blue">Join the Campaign</a>
+
+                </div>
+            </div>
+        </div><!-- .donate-card -->
+
+        <div class="donate-card">
+        <img src="img/demo.jpg" height="" width="480" />
+            <div class="donate-card-inner">
+                <div class="card-description">
+                    <h3>Manchester United Junior High School</h3>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div><!-- .progress -->
+
+                    <div class="progress-stats">
+                        <div class="row">
+                            <div class="col">
+                                Raised (25%)
+                            </div>
+                            <div class="col text-right">
+                                Goal
+                            </div>
+                        </div>
+                    </div><!-- .progress-stats -->
+
+                        <a href="donate.php" class="btn btn-blue">Join the Campaign</a>
+
+                </div>
+            </div>
+        </div><!-- .donate-card -->
+        <div class="donate-card">
+        <img src="img/demo.jpg" height="" width="480" />
+            <div class="donate-card-inner">
+                <div class="card-description">
+                    <h3>Title</h3>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div><!-- .progress -->
+
+                    <div class="progress-stats">
+                        <div class="row">
+                            <div class="col">
+                                Raised
+                            </div>
+                            <div class="col text-right">
+                                Goal
+                            </div>
+                        </div>
+                    </div><!-- .progress-stats -->
+
+                        <a href="donate.php" class="btn btn-blue">Join the Campaign</a>
+
+                </div>
+            </div>
+        </div><!-- .donate-card -->
+                <div class="donate-card">
+        <img src="img/demo.jpg" height="" width="480" />
+            <div class="donate-card-inner">
+                <div class="card-description">
+                    <h3>Title</h3>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div><!-- .progress -->
+
+                    <div class="progress-stats">
+                        <div class="row">
+                            <div class="col">
+                                Raised
+                            </div>
+                            <div class="col text-right">
+                                Goal
+                            </div>
+                        </div>
+                    </div><!-- .progress-stats -->
+
+                        <a href="donate.php" class="btn btn-blue">Join the Campaign</a>
+
+                </div>
+            </div>
+        </div><!-- .donate-card -->
+                <div class="donate-card">
+        <img src="img/demo.jpg" height="" width="480" />
+            <div class="donate-card-inner">
+                <div class="card-description">
+                    <h3>Title</h3>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div><!-- .progress -->
+
+                    <div class="progress-stats">
+                        <div class="row">
+                            <div class="col">
+                                Raised
+                            </div>
+                            <div class="col text-right">
+                                Goal
+                            </div>
+                        </div>
+                    </div><!-- .progress-stats -->
+
+                        <a href="donate.php" class="btn btn-blue">Join the Campaign</a>
+
+                </div>
+            </div>
+        </div><!-- .donate-card -->
+    </div>
+</div>
+
+<?php include( './inc/footer.php'); ?>
---
+++ start-a-campaign.php
@@ -0,0 +1,55 @@
+<?php include( './inc/header.php'); ?>
+
+
+<h1 class="text-center">Start a Campaign</h1>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-5 offset-md-1">
+            <div class="card">
+                <img src="https://d1dvsr1i84olon.cloudfront.net/wp-content/uploads/2017/07/21051252/get-dig-pink.jpg" alt="" height="480" width="480">
+
+                <div class="card-text">
+                    <h3>Host a Dig Pink&reg; Tournament</h3>
+                    <p>
+                        Start a new tournament or turn one of your
+                        existing tournaments into a Dig Pink&reg; event.
+                        Either way you can support the cause.
+                    </p>
+                    <p>
+                        <a href="#" class="card-cta">Start a dig pink tournament</a>
+                    </p>
+                </div>
+
+                <a href="#" class="click">
+                    <span class="screen-reader-text">Host a Dig Pink&reg; Tournament</span>
+                </a>
+            </div>
+        </div>
+        <div class="col-md-5">
+            <div class="card">
+                <img src="https://d1dvsr1i84olon.cloudfront.net/wp-content/uploads/2017/07/21051249/get-ambassador.jpg" alt="Ambassador program" height="480" width="480">
+
+                <div class="card-text blue">
+                    <h3>Host a Dig Pink Event at Your School</h3>
+                    <p>
+                        Dig Pink&reg; events can be organized at the local,
+                        county, city or state level.
+                    </p>
+                    <p>
+                        <a href="#" class="card-cta">Start a dig pink event</a>
+                    </p>
+                </div>
+                <a href="#" class="click">
+                    <span class="screen-reader-text">Host a Dig Pink® Event at Your School</span>
+                </a>
+            </div>
+        </div>
+    </div>
+    <h2 class="text-center">Want to create your own campaign?</h2>
+    <p class="text-center">
+        <a href="" class="btn">Start Your Campaign</a>
+    </p>
+</div>
+
+<?php include( './inc/footer.php'); ?>
