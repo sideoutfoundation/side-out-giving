244c2ed2219bf885afe856e104cd7153aeb0bb7b
fsimmons <fsimmons@gmail.com>
2018-06-28 10:02:27 -0500

    modal changes

--- inc/theme.php
+++ inc/theme.php
@@ -1595,30 +1595,15 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-lg-6">
-            <h2>Modals</h2>
-            <div class="bs-component">
-              <div class="modal">
-                <div class="modal-dialog" role="document">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h5 class="modal-title">Modal title</h5>
-                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                      </button>
-                    </div>
-                    <div class="modal-body">
-                      <p>Modal body text goes here.</p>
-                    </div>
-                    <div class="modal-footer">
-                      <button type="button" class="btn btn-primary">Save changes</button>
-                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <div class="col-lg-6"><h2>Modals</h2> <button type="button" data-toggle="modal" data-target="#exampleModal" class="btn btn-primary">
+                        Launch demo modal
+                    </button> <div id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" class="modal fade"><div role="document" class="modal-dialog"><div class="modal-content"><div class="modal-header"><h5 id="exampleModalLabel" class="modal-title">Modal title</h5> <button type="button" data-dismiss="modal" aria-label="Close" class="close"><span aria-hidden="true">×</span></button></div> <div class="modal-body">
+                                    ...
+                                </div> <div class="modal-footer"><button type="button" data-dismiss="modal" class="btn btn-secondary">Close</button> <button type="button" class="btn btn-primary">Save changes</button></div></div></div></div> <button type="button" data-toggle="modal" data-target=".bd-example-modal-lg" class="btn btn-primary">Large modal</button> <div tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true" class="modal fade bd-example-modal-lg"><div class="modal-dialog modal-lg"><div class="modal-content"><div class="modal-header"><h4 id="myLargeModalLabel" class="modal-title">Large modal</h4> <button type="button" data-dismiss="modal" aria-label="Close" class="close"><span aria-hidden="true">×</span></button></div>
+                                ...
+                            </div></div></div> <button type="button" data-toggle="modal" data-target=".bd-example-modal-sm" class="btn btn-primary">Small modal</button> <div tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true" class="modal fade bd-example-modal-sm"><div class="modal-dialog modal-sm"><div class="modal-content"><div class="modal-header"><h4 id="mySmallModalLabel" class="modal-title">Small modal</h4> <button type="button" data-dismiss="modal" aria-label="Close" class="close"><span aria-hidden="true">×</span></button></div>
+                                ...
+                            </div></div></div> <div class="bs-component"><div class="modal"><div role="document" class="modal-dialog"><div class="modal-content"><div class="modal-header"><h5 class="modal-title">Modal title</h5> <button type="button" data-dismiss="modal" aria-label="Close" class="close"><span aria-hidden="true">×</span></button></div> <div class="modal-body"><p>Modal body text goes here.</p></div> <div class="modal-footer"><button type="button" class="btn btn-primary">Save changes</button> <button type="button" data-dismiss="modal" class="btn btn-secondary">Close</button></div></div></div></div></div></div>
           <div class="col-lg-6">
             <h2>Popovers</h2>
             <div class="bs-component" style="margin-bottom: 3em;">
