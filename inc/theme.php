cc442cb46acd9939a8ce75502dcac252816119e1
fsimmons <fsimmons@gmail.com>
2018-07-10 09:35:17 -0500

    required field * label element

--- inc/theme.php
+++ inc/theme.php
@@ -754,7 +754,7 @@
                       <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com">
                     </div>
                   </div>
-                  <div class="form-group">
+                  <div class="form-group required">
                     <label for="exampleInputEmail1">Email address</label>
                     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
                     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
